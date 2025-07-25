3f26521e098b28fb6b39b368100614a05060b2c2
telephrag <trofimov.nikita20@gmail.com>
2025-07-25 17:18:08 +0000

    Made searching build schema file more robust. Allowed for custom build schema name.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"io/fs"
 	"log"
@@ -18,29 +19,22 @@ func init() {
 
 }
 
-func Run(wd string, ef func(error)) {
+func Run(workDir, buildSchemaFileName string, ef func(error)) {
 
-	if err := os.Chdir(wd); err != nil {
+	workDir, err := filepath.Abs(workDir)
+	if err != nil {
+		log.Fatalf("invalid path to project: %s\n", err)
+	}
+
+	if err := os.Chdir(workDir); err != nil {
 		log.Fatal(err)
 	}
 
 	// Just file realted shit. Scroll until the next comment.
-	buildFile, err := os.Open("build.json")
-	if err != nil {
-		log.Fatal("build.json not found")
-	}
-
-	contents, err := io.ReadAll(buildFile)
-	if err != nil {
-		log.Fatalf("couldn't read build.json: %s\n", err)
-	}
-
-	buildSchema := map[string]string{}
-	if err := json.Unmarshal(contents, &buildSchema); err != nil {
-		log.Fatalf("failed to unmarshal build.json: %s\n", err)
-	}
-
+	var buildFilePath string
+	var buildFileFound bool
 	srcPaths, srcNames := []string{}, []string{}
+
 	err = filepath.WalkDir(".",
 		func(path string, d fs.DirEntry, err error) error {
 			if err != nil {
@@ -51,14 +45,43 @@ func Run(wd string, ef func(error)) {
 				srcPaths = append(srcPaths, path)
 				srcNames = append(srcNames, d.Name())
 			}
+
+			if d.Name() == buildSchemaFileName {
+				if buildFileFound {
+					log.Fatalf("another %s found at %s\n", buildSchemaFileName, path)
+				}
+
+				buildFilePath += path
+				buildFileFound = true
+
+			}
+
 			return nil
 		})
 	if err != nil {
-		log.Fatalf("errored while looking for files to compile: %s\n", err)
+		log.Fatalf("error walking project directory: %s\n", err)
 	}
 
-	lf := len(srcPaths)
-	lb := len(buildSchema)
+	if !buildFileFound {
+		log.Fatalf("%s not found\n", buildSchemaFileName)
+	}
+
+	buildFile, err := os.Open(buildFilePath)
+	if err != nil {
+		log.Fatalf("failed to open %s: %s\n", buildFilePath, err)
+	}
+
+	contents, err := io.ReadAll(buildFile)
+	if err != nil {
+		log.Fatalf("couldn't read %s: %s\n", buildFilePath, err)
+	}
+
+	buildSchema := map[string]string{}
+	if err := json.Unmarshal(contents, &buildSchema); err != nil {
+		log.Fatalf("failed to unmarshal %s: %s\n", buildFilePath, err)
+	}
+
+	lf, lb := len(srcPaths), len(buildSchema)
 	if lf != lb {
 		log.Fatalf("build schema: %d entries; files found: %d\n", lb, lf)
 	}
@@ -66,6 +89,7 @@ func Run(wd string, ef func(error)) {
 	// Actual work starts here
 	hoodies := make([]*hoodie.Hoodie, len(srcPaths))
 	for i := range srcPaths {
+
 		f, err := os.Open(srcPaths[i])
 		if err != nil {
 			log.Fatalf("failed to open .hoo file %s: %s\n", srcPaths[i], err)
@@ -98,25 +122,33 @@ func Run(wd string, ef func(error)) {
 
 func main() {
 	var errFunc = func(err error) { log.Fatal(err) }
-	var workDir string
 
-	workDir = *flag.String("d", ".", "Specifies sourcecode directory.")
-
-	flag.BoolFunc("c", "Specifies wether to continue parsing project on error in one of the files.", func(string) error {
-		errFunc = func(err error) { log.Println(err) }
-		return nil
-	})
-
+	var workDir = flag.String("d", ".", "Sourcecode directory.")
+	var buildSchemaFileName = flag.String("s", "build.json", "Build schema file's name.")
+	flag.BoolFunc("c", "Wether to continue parsing project on error.",
+		func(string) error {
+			errFunc = func(err error) { log.Println(err) }
+			return nil
+		},
+	)
 	flag.Parse()
-	Run(workDir, errFunc)
+
+	fmt.Println(*workDir)
+
+	Run(*workDir, *buildSchemaFileName, errFunc)
 }
 
 // Do all errors contain src path?
 
-// TODO: Improve error handling. Provide line #
-// 	where error has occured in all use-cases.
-// 		write test or files where error is supposed to occure
+// DONE: Problems finding build.json, try ./main test_input/project/
+// DONE: Allow passing name of .json build schema
+
+// TODO: Improve error handling.
+//	> Provide line # where error has occured in all use-cases.
+// 	> Write test or files where error is supposed to occure
+//  > Return specific error types to be more concise
 // TODO: Make tabulation in output files pretty
 // TODO: Compile each file in a seperate thread
 // TODO: Allow the same output destination for multiple .hoo files
 // TODO: Allow #include and #base statements
+// 	> why? hoodie does the same but differently
